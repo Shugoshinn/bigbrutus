45b114b48a073083c6b8ec51c8c5279d2f43763f
Nicolas <nico.hernandezr@duocuc.cl>
2026-05-18 22:53:14 -0400

    bases de datos listas

---
+++ cliente/src/main/resources/db.migration/V1__create_table_clientes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE clientes (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    fecha_registro DATETIME NOT NULL,
+    activo BOOLEAN NOT NULL DEFAULT TRUE
+);
