fb8b0957fe7e3275a00d93da97577f82b189a79b
PAAWSA02LC1314 <Duoc@PAAWSA02LC1314.duoc.acad>
2026-06-16 13:30:48 -0400

    todo dockerizado y el docker-compose echo

---
+++ init.db/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE bd_clientes;
+CREATE DATABASE bd_cocineros;
+CREATE DATABASE bd_pedidos;
+CREATE DATABASE bd_productos;
+CREATE DATABASE bd_repartidores;
+CREATE DATABASE bd_sucursales;
+CREATE DATABASE bd_vehiculos;
+CREATE DATABASE bd_vendedores;
